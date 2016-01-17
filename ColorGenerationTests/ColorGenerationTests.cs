1fc4fc1d78c5da00b203dc620d623ccb637557b7
Al Gardner <al@alski.net>
2016-01-17 23:29:14 +0000

    Add initial color generation

---
+++ ColorGenerationTests/ColorGenerationTests.cs
@@ -0,0 +1,79 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Should;
+using System.Windows.Media;
+using ColourGeneration;
+
+namespace ColorGenerationTests
+{
+    [TestFixture]
+    public class ThatColourGenerator
+    {
+     
+        [Test]
+        public void ShouldBeFullRedAndRampGreenFrom0To255()
+        {
+            var rampUp = Enumerable.Range(0, 255);
+            var colours = from x in rampUp let y = ColorGenerator.FromIndex(x) let z = (byte)(x) select new { y, z };
+            colours.All(result => result.y.R == 255).ShouldBeTrue();
+            colours.All(result => result.y.G == result.z).ShouldBeTrue();
+            colours.All(result => result.y.B == 0).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldBeFullGreenAndRampDownRedFrom256To511()
+        {
+            var rampUp = Enumerable.Range(0, 255);
+            var colours = from x in rampUp let y = ColorGenerator.FromIndex(x + 256) let z = (byte)(255 - x) select new { y, z };
+            colours.All(result => result.y.R == result.z).ShouldBeTrue();
+            colours.All(result => result.y.G == 255).ShouldBeTrue();
+            colours.All(result => result.y.B == 0).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldBeFullGreenAndRampUpBlueFrom512To767()
+        {
+            var rampUp = Enumerable.Range(0, 255);
+            var colours = from x in rampUp let y = ColorGenerator.FromIndex(x + 512) let z = (byte)(x) select new { y, z };
+            colours.All(result => result.y.R == 0).ShouldBeTrue();
+            colours.All(result => result.y.G == 255).ShouldBeTrue();
+            colours.All(result => result.y.B == result.z).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldBeFullBlueAndRampDownGreenFrom768To1023()
+        {
+            var rampUp = Enumerable.Range(0, 255);
+            var colours = from x in rampUp let y = ColorGenerator.FromIndex(x + 768) let z = (byte)(255 - x) select new { y, z };
+            colours.All(result => result.y.R == 0).ShouldBeTrue();
+            colours.All(result => result.y.G == result.z).ShouldBeTrue();
+            colours.All(result => result.y.B == 255).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldBeFullBlueAndRampUpRedFrom1024To1279()
+        {
+            var rampUp = Enumerable.Range(0, 255);
+            var colours = from x in rampUp let y = ColorGenerator.FromIndex(x + 1024) let z = (byte)(x) select new { y, z };
+            colours.All(result => result.y.R == result.z).ShouldBeTrue();
+            colours.All(result => result.y.G == 0).ShouldBeTrue();
+            colours.All(result => result.y.B == 255).ShouldBeTrue();
+        }
+
+        [Test]
+        public void ShouldBeFullRedAndRampDownBlueFrom1280To1535()
+        {
+            var rampUp = Enumerable.Range(0, 255);
+            var colours = from x in rampUp let y = ColorGenerator.FromIndex(x + 1280) let z = (byte)(255 - x) select new { y, z };
+            colours.All(result => result.y.R == 255).ShouldBeTrue();
+            colours.All(result => result.y.G == 0).ShouldBeTrue();
+            colours.All(result => result.y.B == result.z).ShouldBeTrue();
+        }
+
+
+    }
+}
---
+++ ColourGeneration/ColourGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ColourGeneration
+{
+    public static class ColorGenerator
+    {
+        public static Color FromIndex(int index)
+        {
+            byte red = 0;
+            byte green = 0;
+            byte blue = 0;
+
+            red = CalculateColor(index, -256, 256, 512, 1024);
+            green = CalculateColor(index, 256, 768, 1024, 0);
+            blue = CalculateColor(index, 768, 1280, 0, 512);
+
+            return Color.FromArgb(0, red, green, blue);
+        }
+
+        private static byte CalculateColor(int index, int fullColorStart, int rampDownStart, int noColorStart, int rampUpStart)
+        {
+            if (index >= fullColorStart && index < fullColorStart + 512)
+            {
+                return 255;
+            }
+
+            if (index >= rampDownStart && index < rampDownStart + 256)
+            {
+                return (byte)(rampDownStart + 255 - index);
+            }
+
+            if (index >= rampUpStart && index < rampUpStart + 256)
+            {
+                return (byte)(index - rampUpStart);
+            }
+
+            if (index >= noColorStart && index < noColorStart + 512)
+            {
+                return 0;
+            }
+
+            if (fullColorStart < 0
+                && index >= fullColorStart + 1536 && index < 1536)
+            {
+                return 255;
+            }
+
+            throw new IndexOutOfRangeException(index + " is out of known range");
+        }
+    }
+}
